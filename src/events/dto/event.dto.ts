6102212f73bad499117efce193220d993fd5886d
Jiwon-Woo <jwoo@student.42seoul.kr>
2023-11-11 23:37:21 +0900

    fix: add isMatching field to EventDto
    #10

--- src/events/dto/event.dto.ts
+++ src/events/dto/event.dto.ts
@@ -17,7 +17,7 @@ export class EventDto {
   @ApiProperty({ description: '이벤트를 생성한 유저의 닉네임' })
   intraId: string;
 
-  @ApiProperty({ description: '이벤트가 매칭 되었는지 여부' })
+  @ApiProperty({ type: Number, description: '이벤트가 매칭 되었는지 여부' })
   isMatching: 0 | 1;
 
   @ApiProperty({ description: '이벤트의 카테고리 id' })
@@ -30,6 +30,7 @@ export class EventDto {
     eventDto.description = eventEntity.description;
     eventDto.createdId = eventEntity.createUserId;
     eventDto.intraId = 'any';
+    eventDto.isMatching = eventEntity.matchedAt ? 1 : 0;
     eventDto.categoryId = 0;
     return eventDto;
   }
