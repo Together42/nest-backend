e2fa7a3fd49a5b92e8f099aeeb16002759c80dcd
seo-wo <4nyth4me@gmail.com>
2023-11-15 14:28:24 +0900

    feat: use ConfigModule
    for using env file easily, implement configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,39 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev',
+      isGlobal: true,
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService)=>({
+        type: 'mysql',
+        host: configService.get('database.DB_HOST'),
+        port: configService.get('database.DB_PORT'),
+        username: configService.get('database.DB_USER'),
+        password: configService.get('database.DB_PASSWORD'),
+        database: configService.get('database.DB_DATABASE'),
+        entities: ['dist/**/*.entity.js'],
+        synchronize: configService.get('database.DB_SYNC') === 'true',
+        namingStrategy: new SnakeNamingStrategy(),
+      }),
+    }),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/configuration.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+
+
+export default () => ({
+  database: {
+    DB_HOST: process.env.MYSQL_HOST || 'localhost',
+    DB_PORT: process.env.MYSQL_PORT || '3306',
+    DB_USER: process.env.MYSQL_USER || 'default',
+    DB_PASSWORD: process.env.MYSQL_PASSWORD || 'default',
+    DB_DATABASE: process.env.MYSQL_DATABASE || 'default',
+    DB_SYNC: process.env.NODE_ENV === 'prod' ? 'false' : 'true',
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET || 'default',
+    expiresIn: process.env.JWT_EXPIRES_IN || '1d',
+  },
+  google: {
+    clientId: process.env.GOOGLE_CLIENT_ID || 'default',
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET || 'default',
+    callbackUrl: process.env.GOOGLE_REDIRECT_URI || 'default',
+    prompt: process.env.GOOGLE_PROMPT || '',
+  },
+});
