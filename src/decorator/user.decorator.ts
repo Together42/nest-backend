c9f21cfc3d095b307bc58c39e0a6abb35d46f394
seo-wo <4nyth4me@gmail.com>
2023-11-15 14:29:55 +0900

    feat: implment user decorator

---
+++ src/decorator/user.decorator.ts
@@ -0,0 +1,11 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from '../user/entity/user.entity';
+
+export const GetUser = createParamDecorator((
+  data: unknown,
+  context: ExecutionContext, 
+): User => {
+  const req = context.switchToHttp().getRequest();
+  return req.user;
+}
+)
