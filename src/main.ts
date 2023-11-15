74d9b68b865369150ed342d4830b949f0d215213
seo-wo <4nyth4me@gmail.com>
2023-11-15 14:29:26 +0900

    feat: set cors
    connect to frontend for test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common'; 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  })
+
+  app.setGlobalPrefix('api');
+  const port = process.env.BACK_PORT || 4242;
+  await app.listen(port);
+  Logger.log(`Server running on ${port}`)
 }
 bootstrap();
