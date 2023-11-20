10c24c55a3e449c1a5446f6c97c3ca3fdcf10e60
Jiwon-Woo <jwoo@student.42seoul.kr>
2023-11-20 21:47:11 +0900

    feat: add temp UserEntity
    #10

---
+++ src/meetups/entity/user.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('user')
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false, unique: true })
+  googleEmail: string;
+
+  @Column({ nullable: false, unique: true })
+  nickname: string;
+
+  @Column({ nullable: true })
+  slackMemberId: string;
+
+  @Column({ nullable: true })
+  profileImageUrl: string;
+
+  @Column({ nullable: false })
+  role: number;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ type: 'timestamp' })
+  deletedAt: Date;
+}
