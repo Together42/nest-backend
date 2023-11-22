717ba0698c036a0904ce49e92f8e10c48e1fab1f
seo-wo <4nyth4me@gmail.com>
2023-11-22 16:16:06 +0900

    feat: adjust env value for fornt url
    #6

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONT_URL || 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
