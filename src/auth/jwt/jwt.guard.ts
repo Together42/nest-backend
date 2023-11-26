3d3928b8fdd00d5b697f1b2ced40b3db47ead099
sideseal <ckgun@naver.com>
2023-11-26 22:27:38 +0900

    modify: prettier jwt.guard.ts

--- src/auth/jwt/jwt.guard.ts
+++ src/auth/jwt/jwt.guard.ts
@@ -5,14 +5,21 @@ import { AuthGuard } from '@nestjs/passport';
 export class JwtGuard extends AuthGuard('jwt') {
   private readonly logger = new Logger(JwtGuard.name);
 
-  async canActivate(context: ExecutionContext): Promise<boolean>{
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     if (context.getType() === 'http') {
-      this.logger.debug(`canActivate [http, ${context.getArgs()[0].method} ${context.getArgs()[0].url}]`);
-    } else{
+      this.logger.debug(
+        `canActivate [http, ${context.getArgs()[0].method} ${
+          context.getArgs()[0].url
+        }]`,
+      );
+    } else {
       this.logger.debug(`canActivate [not http]`);
       return false;
     }
-    const token = context.switchToHttp().getRequest().headers?.authorization?.split('Bearer ')[1];
+    const token = context
+      .switchToHttp()
+      .getRequest()
+      .headers?.authorization?.split('Bearer ')[1];
     this.logger.debug(`canActivate [token: ${token}]`);
 
     let result: boolean = false;
