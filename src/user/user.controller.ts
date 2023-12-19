bd07236ac12c157588b01a65a0c98b59dc5acee0
Jiwon-Woo <jwoo@student.42seoul.kr>
2023-12-19 17:28:18 +0900

    feat: add api that toggle user isActive
    #49

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,26 @@
-import { UseGuards, Controller, Get, Logger } from '@nestjs/common';
+import { UseGuards, Controller, Get, Logger, Patch, Body } from '@nestjs/common';
 import { UserService } from './user.service';
 import { GetUser } from 'src/decorator/user.decorator';
 import { JwtGuard } from 'src/auth/guard/jwt.guard';
 import { RoleGuard } from 'src/auth/guard/role.guard';
 import { Role } from 'src/decorator/role.decorator';
 import UserRole from './enum/user.enum';
-import { ApiBearerAuth, ApiInternalServerErrorResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { JwtRefreshGuard } from 'src/auth/guard/jwt-refresh.guard';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiExtraModels,
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  getSchemaPath,
+} from '@nestjs/swagger';
 import { InternalServerExceptionBody } from 'src/common/dto/error-response.dto';
 import { UserRankingDto } from './dto/user-ranking.dto';
+import {
+  UpdateUserActivityByIdDto,
+  UpdateUserActivityByNameDto,
+} from './dto/update-user-activity.dto';
 
 @Controller('users')
 @ApiTags('users')
@@ -18,11 +30,11 @@ export class UserController {
 
   @Get('test')
   @UseGuards(JwtGuard, RoleGuard)
+  @Role([UserRole.ADMIN])
   @ApiBearerAuth()
-  @Role([UserRole.USER, UserRole.LIBRARIAN, UserRole.ADMIN])
   async jwtAuthTest(@GetUser() user) {
     this.logger.debug(`jwtAuthTest [user: ${JSON.stringify(user)}]`);
-    return 'jwtAuthTest';
+    return user;
   }
 
   @Get('ranking')
@@ -31,5 +43,29 @@ export class UserController {
   @ApiInternalServerErrorResponse({ type: InternalServerExceptionBody })
   async findUserRanking(): Promise<UserRankingDto[]> {
     return await this.userService.getUserRanking();
+  }
+
+  @Patch('activity')
+  @UseGuards(JwtGuard, RoleGuard)
+  @Role([UserRole.ADMIN])
+  @ApiBearerAuth()
+  @ApiExtraModels(UpdateUserActivityByIdDto, UpdateUserActivityByNameDto)
+  @ApiBody({
+    description: '유저 고유 아이디 혹은 유저의 닉네임을 통해 유저의 isActive를 수정합니다.',
+    schema: {
+      oneOf: [
+        { $ref: getSchemaPath(UpdateUserActivityByIdDto) },
+        { $ref: getSchemaPath(UpdateUserActivityByNameDto) },
+      ],
+    },
+    examples: {
+      'By userId': { value: { userId: 1, isActive: false } },
+      'By nickname': { value: { nickname: 'jwoo', isActive: true } },
+    },
+  })
+  async updateUserActivity(
+    @Body() updateUserActiviyDto: UpdateUserActivityByIdDto | UpdateUserActivityByNameDto,
+  ) {
+    return await this.userService.updateUserActivity(updateUserActiviyDto);
   }
 }
